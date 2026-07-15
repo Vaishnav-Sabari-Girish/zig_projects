908f22714b107a80f978a426e60ac5c34a87a9fd
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-07-15 22:56:38 +0530

    feat(calc): Basic calculator implemented
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- rpn_calculator/src/root.zig
+++ rpn_calculator/src/root.zig
@@ -2,17 +2,32 @@
 const std = @import("std");
 const Io = std.Io;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
 
 pub fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
-test "basic add functionality" {
+pub fn sub(a: i32, b: i32) i32 {
+    return a - b;
+}
+
+pub fn prod(a: i32, b: i32) i32 {
+    return a * b;
+}
+
+pub fn quo(a: f32, b: f32) f32 {
+    return a / b;
+}
+
+pub fn rem(a: u32, b: u32) u32 {
+    return a % b;
+}
+
+
+test "basic calculator functionalities" {
     try std.testing.expect(add(3, 7) == 10);
+    try std.testing.expect(sub(7, 3) == 4);
+    try std.testing.expect(prod(7, 3) == 21);
+    try std.testing.expect(quo(8.0, 2.0) == 4.0);
+    try std.testing.expect(rem(10, 6) == 4);
 }
